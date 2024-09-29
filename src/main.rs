95e064c0c3e932b0e92e773594b7b9ec5d91edc1
jeka1331 <47132602+jeka1331@users.noreply.github.com>
2024-09-29 12:53:31 +0500

    Корректный вывод шаблона (#1)
    * Добавлен файл рендера боевого расчета в .gitignore
    
    * Сделан корректный вывод шаблона и подготовлен код для заполнения шаблона
    
    ---------
    
    Co-authored-by: evg <mrjeka111@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,68 @@
-use std::{clone, collections::HashMap};
 use std::error::Error;
-use std::{fs, vec};
-use tera::{Context, Tera};
 
-use boevoi::{read_positions_from_toml, read_soldiers_from_toml, render_template_to_file, Position, Soldier};
+use boevoi::{
+    read_positions_from_toml, read_soldiers_from_toml, render_template_to_file, AssignedPosition,
+    AssignedPositions, DutyGroupMember, DutyGroupMembers, FireGroupMember, FireGroupMembers,
+    Position, Soldier,
+};
+use chrono::{DateTime, Duration, Local};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // // Пример данных
-    // let mut people = HashMap::new();
-    // people.insert("name".to_string(), "John Doe".to_string());
-    // people.insert("age".to_string(), "30".to_string());
+    let now: DateTime<Local> = Local::now();
 
-    // // Создание вектора для примера
-    // let mut people_vec: Vec<HashMap<String, String>> = Vec::new();
-    // people_vec.push(people);
-
-    // let mut data = HashMap::new();
-    // data.insert("people", people_vec);
-
-    // // Вызов функции для рендеринга и сохранения
-    // render_template_to_file(
-    //     "templates/**/*", // Путь к шаблонам
-    //     "table.html",     // Имя шаблона
-    //     "output.html",    // Путь для сохранения HTML
-    //     &data,
-    // )
+    // Чтение с файлов
     let soldiers = read_soldiers_from_toml("data/soldiers.toml")?;
     for value in &soldiers.soldiers {
         println!("Soldier: {:?}", value);
-        
     }
 
     let positions = read_positions_from_toml("data/positions.toml")?;
     for value in &positions.positions {
         println!("Soldier: {:?}", value);
-        
     }
 
-    let mut assigned_positions: Vec<(Position, Vec<Soldier>)> = Vec::new();
+    let mut duty_group_members: DutyGroupMembers = Vec::new();
+    let mut assigned_positions: AssignedPositions = Vec::new();
+    let mut fire_group_members: FireGroupMembers = Vec::new();
+
+    fire_group_members.push(FireGroupMember {
+        rank: "рядовой".to_string(),
+        fio: "Kural".to_string(),
+        vzvod: "VNV".to_string(),
+    });
+
+    assigned_positions.push(AssignedPosition {
+        position: Position {
+            name: "Штаб".to_string(),
+            ammo: "БКАР".to_string(),
+            member_count: 1,
+            vzvod_priority: "kv".to_string(),
+        },
+        member: Soldier {
+            rank: "рядовой".to_string(),
+            fio: "Kural".to_string(),
+            vzvod: "VNV".to_string(),
+        },
+    });
+
+    duty_group_members.push(DutyGroupMember {
+        rank: "рядовой".to_string(),
+        fio: "Kural".to_string(),
+        vzvod: "VNV".to_string(),
+    });
+
+    // Вызов функции для рендеринга и сохранения
+    let _ = render_template_to_file(
+        "templates/**/*", // Путь к шаблонам
+        "table.html",     // Имя шаблона
+        "output.html",
+        assigned_positions, // Путь к файлу для вывода
+        duty_group_members,
+        fire_group_members,
+        &now.format("%d.%m.%Y").to_string(),
+        &(now + Duration::days(1)).format("%d.%m.%Y").to_string(),
+    );
+
+    // let mut assigned_positions: Vec<(Position, Vec<Soldier>)> = Vec::new();
     Ok(())
 }
